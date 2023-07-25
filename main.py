66e474f9858f1cbcc607bc5845b897d79706da9b
Siripoom <artorsiriratpoom@gmail.com>
2023-07-25 21:42:48 +0700

    commmit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-
+import datetime
 # * READ IMAGE
 # file = "D:\Python-Basic\OpenCV\image\ds_wordcloud.png" #? get image file path to file(variable)
 # img = cv2.imread(file)  #? read image file path with .imread(image read function)
@@ -43,10 +43,10 @@ import cv2
 # cv2.release()
 # cv2.destroyAllWindows()
 
-#* Video record .VideoWrite is function for save new video
-#todo1 Open camera
-#todo2 Record video
-#todo3 Save as a new video and type(mp4)
+# * Video record .VideoWrite is function for save new video
+# todo1 Open camera
+# todo2 Record video
+# todo3 Save as a new video and type(mp4)
 # cap = cv2.VideoCapture(0)   #? todo change number according to your webcam
 # fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # result = cv2.VideoWriter("output.avi",fourcc,20.0,(640,480)) #? setting video
@@ -62,16 +62,46 @@ import cv2
 # result.release()
 # cv2.release()
 # cv2.destroyAllWindows()
-#* Draw Line/Arrow
-#todo read image
-file = "D:\Python-Basic\OpenCV\image\ds_wordcloud.png"
-img = cv2.imread(file)
-#todo edit image
-img = cv2.resize(img,(700,700))
-#todo draw line
-#? line (image, start(x,y), end(x,y), color(BRG), weight)
-cv2.arrowedLine(img,(700,700),(100,100),(255,0,0),3)
-cv2.imshow("Image",img)
+# * Draw Line/Arrow
+# todo read image
+# file = "D:\Python-Basic\OpenCV\image\ds_wordcloud.png"
+# img = cv2.imread(file)
+# todo edit image
+# img = cv2.resize(img,(700,700))
+# todo draw line
+# ? line (image, start(x,y), end(x,y), color(BRG), weight)
+# cv2.arrowedLine(img,(700,700),(100,100),(255,0,0),3)
+# todo draw rectangle
+# ? ractangle(image, มุมที่1ซ้ายบน, มุมที่2ล่างขวา, color, weight)
+# cv2.rectangle(img, (0,0),(500,500),(255,0,0),2) #weight -1
+# todo draw circle
+# ? .circle(image, center(x,y)),radius,color(BRG),thickness)
+# cv2.circle(img,(500,500),70,(255,0,0),3)
+# todo put Text
+# ?  .putText(image,text,coordinates(x,y),font,size,color,weight)
+# cv2.putText(img,"Human",(400,400),2,2,(255,0,0),4)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("Image",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# * Show date-time in video
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+result = cv2.VideoWriter("OutputwithDateTime.avi",fourcc,20.0,(640,480))
+while cap.isOpened():
+    check, frame = cap.read()
+
+    if check == True:
+        currentDate = str(datetime.datetime.now())
+        cv2.putText(frame,currentDate,(10,30),1,2,(0,0,0),2)
+        cv2.imshow("Output", frame)
+        result.write(frame)
+        if cv2.waitKey(1) & 0xFF == ord("e"):
+            break
+    else:
+        break
+
+result.release();
+cap.release();
+cv2.destroyAllWindows();
